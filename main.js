7acc30d9f15ffadb20cf81f23fcd1bbc9855f7c7
Ryotarosu22 <96615807+Ryotarosu22@users.noreply.github.com>
2023-05-05 11:11:21 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+// ウェブページの読み込みが完了したら実行する
+window.onload = function() {
+  // ボタン要素を取得する
+  var button = document.getElementById('button');
+
+  // ボタンがクリックされたら、アラートを表示する
+  button.onclick = function() {
+    alert('Hello World!');
+  };
+};
