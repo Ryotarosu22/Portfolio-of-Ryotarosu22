d9a4aae744bcfcace23651c8795eecb6819d5c2a
Ryotarosu22 <96615807+Ryotarosu22@users.noreply.github.com>
2023-05-05 10:48:21 +0900

    Create header.jsx

---
+++ header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#about">自己紹介</a></li>
+          <li><a href="#skills">スキルセット</a></li>
+          <li><a href="#projects">プロジェクト</a></li>
+          <li><a href="#contact">お問い合わせ</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
